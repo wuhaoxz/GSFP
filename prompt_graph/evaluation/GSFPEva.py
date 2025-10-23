6bc7e3c3a3fa4ae3cb2ff840d36635594e3b294b
wuhao <96791027+wuhaoxz@users.noreply.github.com>
2025-10-23 19:38:16 +0800

    Add files via upload

---
+++ prompt_graph/evaluation/GSFPEva.py
@@ -0,0 +1,44 @@
+import torchmetrics
+import torch
+from tqdm import tqdm
+
+
+def GSFPEva(loader, gnn, prompt, answering, embedding,num_class, device):
+    gnn.eval()
+    answering.eval()
+    embedding.eval()
+    prompt.eval()
+    accuracy = torchmetrics.classification.Accuracy(task="multiclass", num_classes=num_class).to(device)
+    macro_f1 = torchmetrics.classification.F1Score(task="multiclass", num_classes=num_class, average="macro").to(device)
+    auroc = torchmetrics.classification.AUROC(task="multiclass", num_classes=num_class).to(device)
+    auprc = torchmetrics.classification.AveragePrecision(task="multiclass", num_classes=num_class).to(device)
+
+    accuracy.reset()
+    macro_f1.reset()
+    auroc.reset()
+    auprc.reset()
+    with torch.no_grad(): 
+        for batch_id, batch in enumerate(loader): 
+            batch = batch.to(device) 
+            
+            embedding_x = embedding(batch.x)                 
+            prompt_x = prompt.add(embedding_x)
+                  
+            out = gnn(prompt_x, batch.edge_index, batch.batch)
+            if answering:
+                out = answering(out)  
+            pred = out.argmax(dim=1)  
+
+            acc = accuracy(pred, batch.y)
+            ma_f1 = macro_f1(pred, batch.y)
+            roc = auroc(out, batch.y)
+            prc = auprc(out, batch.y)
+            if len(loader) > 20:
+                print("Batch {}/{} Acc: {:.4f} | Macro-F1: {:.4f}| AUROC: {:.4f}| AUPRC: {:.4f}".format(batch_id,len(loader), acc.item(), ma_f1.item(),roc.item(), prc.item()))
+
+    acc = accuracy.compute()
+    ma_f1 = macro_f1.compute()
+    roc = auroc.compute()
+    prc = auprc.compute()
+       
+    return acc.item(), ma_f1.item(), roc.item(),prc.item()
---
+++ prompt_graph/evaluation/__init__.py
@@ -0,0 +1,5 @@
+from .GpromptEva import GpromptEva
+from .GNNEva import GNNGraphEva, GNNNodeEva
+from .GSFPEva import GSFPEva
+from .AllInOneEva import AllInOneEva
+from .GPPTEva import GPPTEva,GPPTGraphEva
