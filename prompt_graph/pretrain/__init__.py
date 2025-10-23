0bb9d57aecdd962451cccbe7b092d57e9ef80a9a
wuhao <96791027+wuhaoxz@users.noreply.github.com>
2025-10-23 19:39:18 +0800

    Add files via upload

---
+++ prompt_graph/pretrain/__init__.py
@@ -0,0 +1,6 @@
+from.Edgepred_GPPT import Edgepred_GPPT
+from.Edgepred_Gprompt import Edgepred_Gprompt
+from.GraphCL import GraphCL
+from.SimGRACE import SimGRACE
+from.GraphMAE import GraphMAE
+from.DGI import DGI
---
+++ prompt_graph/pretrain/base.py
@@ -0,0 +1,48 @@
+import torch
+from prompt_graph.model import GAT, GCN, GCov, GIN, GraphSAGE, GraphTransformer
+from torch.optim import Adam
+
+class PreTrain(torch.nn.Module):
+    def __init__(self, gnn_type='TransformerConv', dataset_name = 'Cora', hid_dim = 128, gln = 2, num_epoch = 1000, device : int = 5,lr=0.01,decay=0.0001,patience=10):
+        super().__init__()
+        self.device = torch.device('cuda:' + str(device) if torch.cuda.is_available() else 'cpu')
+        self.dataset_name = dataset_name
+        self.gnn_type = gnn_type
+        self.num_layer = gln
+        self.epochs = num_epoch
+        self.hid_dim =hid_dim
+        print(dataset_name)
+        print(hid_dim)
+
+        self.learning_rate = lr
+        self.weight_decay = decay
+        self.count_patience = patience
+    
+    def initialize_gnn(self, input_dim, hid_dim):
+        input_dim = 100
+        if self.gnn_type == 'GAT':
+                self.gnn = GAT(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        elif self.gnn_type == 'GCN':
+                self.gnn = GCN(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        elif self.gnn_type == 'GraphSAGE':
+                self.gnn = GraphSAGE(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        elif self.gnn_type == 'GIN':
+                self.gnn = GIN(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        elif self.gnn_type == 'GCov':
+                self.gnn = GCov(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        elif self.gnn_type == 'GraphTransformer':
+                self.gnn = GraphTransformer(input_dim = input_dim, hid_dim = hid_dim, num_layer = self.num_layer)
+        else:
+                raise ValueError(f"Unsupported GNN type: {self.gnn_type}")
+        print(self.gnn)
+        self.gnn.to(self.device)
+        self.optimizer = Adam(self.gnn.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+
+
+        
+#     def load_node_data(self):
+#         self.data, self.dataset = load4node(self.dataset_name, shot_num = self.shot_num)
+#         self.data.to(self.device)
+#         self.input_dim = self.dataset.num_features
+#         self.output_dim = self.dataset.num_classes
+
